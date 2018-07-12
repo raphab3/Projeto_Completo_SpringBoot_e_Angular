728c0701e18c4650012f48674605ac8c681784c3
Rafael <raphab33@hotmail.com>
2018-07-12 03:05:59 -0300

    Criando Class Item;

---
+++ src/main/java/com/algaworks/vendas/service/VendaService.java
@@ -0,0 +1,11 @@
+package com.algaworks.vendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.algaworks.vendas.repository.Vendas;
+
+public class VendaService {
+	
+	@Autowired
+	private Vendas vendas;
+}
