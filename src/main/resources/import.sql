9f65a2fe151da6b0fdcabe771003bae410239e7b
Rafael <raphab33@hotmail.com>
2018-07-12 02:46:02 -0300

    Criando Class Produto ProdutosRepository e ProdutosResource;

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
 insert into cliente (id, nome) values (1, 'Eletrônicos Eletro');
-insert into cliente (id, nome) values (1, 'Smart Compras');
+insert into cliente (id, nome) values (2, 'Smart Compras');
+
+insert into produto (id, nome, valor) values (1, 'Notebook', 5400.0);
+insert into produto (id, nome, valor) values (2, 'Smartphone', 1800.0);
+insert into produto (id, nome, valor) values (3, 'Tablet', 1100.0);
